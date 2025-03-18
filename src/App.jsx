a39f3aba9e1abc3fa2ab3622ddf519e03f4fbbf0
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-03-18 22:39:07 +0000

    resetting it back

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import rickRollImage from './assets/14-rickroll.jpg'
 import './App.css'
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://youtu.be/dQw4w9WgXcQ?si=p6xz3-YA7OusLlHi" target="_blank">
-          <img src={rickRollImage} className="logo react" alt="Rick Astley" />
+        <a href="https://vite.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="Rick Astley" />
         </a>
       </div>
       <h1>Vite + React + Rick Roll</h1>
