7e885811ff42d0e43203b83b8afda44a5249496b
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-03-25 16:29:18 +0000

    working on text element

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,61 @@ import viteLogo from '/vite.svg'
 import rickRollImage from './assets/14-rickroll.jpg'
 import './App.css'
 import rickText from './assets/rickroll.txt'
+import { createElement } from 'react'
 
-function rickRollText (theInput){
-  if(theInput == ""){
-    return {rickText};
-  }
-  else {
-    return "";
-  }
+const lyrics = [
+  "We're no strangers to love",
+  "You know the rules and so do I",
+  "A full commitment's what I'm thinkin' of",
+  "You wouldn't get this from any other guy",
+  "I just wanna tell you how I'm feeling",
+  "Gotta make you understand",
+  "Never gonna give you up, never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry, never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "We've known each other for so long",
+  "Your heart's been aching, but you're too shy to say it",
+  "Inside, we both know what's been going on",
+  "We know the game and we're gonna play it",
+  "And if you ask me how I'm feeling",
+  "Don't tell me you're too blind to see",
+  "Never gonna give you up, never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry, never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Never gonna give you up, never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry, never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "We've known each other for so long",
+  "Your heart's been aching, but you're too shy to say it",
+  "Inside, we both know what's been going on",
+  "We know the game and we're gonna play it",
+  "I just wanna tell you how I'm feeling",
+  "Gotta make you understand",
+  "Never gonna give you up, never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry, never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Never gonna give you up, never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry, never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Never gonna give you up, never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry, never gonna say goodbye",
+  "Never gonna tell a lie and hurt you"
+];
+
+
+function rickRollText (){
+  const thediv = document.getElementById('containerRickRoll');
+  for (let lyric in lyrics) {
+    const insertP = createElement("p");
+    insertP.textContent = lyric;
+    thediv.appendChild(insertP);
+  } 
 }
 
 function RandomNumber() {
@@ -26,13 +73,15 @@ function RandomNumber() {
 }
 
 function FunnyText () {
-  const [innertext, setInnerText] = useState();
+  const [innertext, setInnerText] = useState(false);
 
   return (
     <>
       <h2>Want to see some funny text? Click below!</h2>
-      <button onClick={() => setInnerText((innertext) => rickRollText(innertext))}>Press Here</button>
-      <p>{innertext}</p>
+      <button onClick={() => setInnerText(!innertext)}>{innertext ? 'Press for funny text' : 'Press to hide funny text'}</button>
+      <div>
+        {setInnerText && <div id='containerRickRoll'></div>}
+      </div>
     </>
   );
 }
@@ -51,17 +100,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + Rick Roll</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <RandomNumber />
       <FunnyText />
     </>
